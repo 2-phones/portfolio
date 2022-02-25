ccb37556ac7f35d504c51a3c7e153ad7762b71f9
쫀구 <87120463+2-phones@users.noreply.github.com>
2022-02-25 17:58:30 +0900

    JS: navbar

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// 스크롤시 navbar transparnet 투명 불투명. 
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+
+
+});
