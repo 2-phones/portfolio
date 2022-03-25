439163d00639d2cb273beccc7f84bf255b1cbc65
쫀구 <87120463+2-phones@users.noreply.github.com>
2022-03-25 10:21:57 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// 스크롤시 navbar transparnet 투명 불투명. 
+// make navbar 스크롤시 navbar transparnet 투명 불투명. 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,4 +11,18 @@ document.addEventListener('scroll', () => {
     }
 
 
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
